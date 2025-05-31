9add5cac48b6e7e59d331fc75ba5290b1b545d89
Ayush Kumar <theofficialayush.kumar@gmail.com>
2025-06-01 01:21:12 +0530

    feat: added about us and our services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import { OurServices } from "./components/Services";
+import { projects } from "./components/text/Services";
+import AboutUs from "./components/AboutUs";
+
+
 
 export default function Home() {
   return (
     <div className="">
       <Navbar/>
+      <AboutUs/>
+      <OurServices items={projects}/>
+
     </div>
   );
 }
